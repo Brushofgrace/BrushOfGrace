d9074f361789a3b4ab9081727fa763a51bba8adb
alexhoadley <alexhoadley@outlook.com>
2025-06-13 20:31:42 -0400

    Fix: Correct casing in import path for Header

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/header';
+import Header from './components/Header';
 import ArtworkGallery from './components/ArtworkGallery';
 import ArtworkModal from './components/ArtworkModal';
 import Footer from './components/Footer'; // Import Footer
